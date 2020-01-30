dcad3d408e535afbca663c141162d6780a9c823e
shivamss454 <shivam.sharma@successive.tech>
2020-01-30 15:58:01 +0530

    feat: Setup of Express (OOps based)

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import Iconfig from './IConfig';
 config();
 const configuration: Iconfig = Object.freeze({
-    port: process.env.port,
+    port: process.env.PORT,
     env: process.env.NODE_ENV
 }
 );
