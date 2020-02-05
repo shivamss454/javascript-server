be4b4cb228593af64f98703a81365b6415c59e63
shivamss454 <shivam.sharma@successive.tech>
2020-02-05 18:03:53 +0530

    feat:generic route validation

--- src/conrollers/trainee/routes.ts
+++ src/conrollers/trainee/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import config from './validation';
 import TraineeController from './controller';
 import { post, put } from 'express/lib/application';
+import validationhandler from '../../libs/routes/validationhandler';
+//import validate from './validation';
 const TraineeRouter = Router();
 
 TraineeRouter.route('/')
-    .get( TraineeController.list)
-    .post(TraineeController.create)
-    .delete(TraineeController.delete)
-    .put(TraineeController.update);
+    .get( validationhandler(config.get), TraineeController.list)
+    .post( validationhandler(config.create), TraineeController.create)
+    .delete( validationhandler(config.delete), TraineeController.delete)
+    .put(validationhandler(config.update), TraineeController.update);
+    TraineeRouter.delete('/:id', validationhandler(config.delete),TraineeController.delete);
 export default TraineeRouter;
--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -4,9 +4,8 @@
     console.log('inside errorhandler');
     console.log('Error' , err);
     const {error, status, message } = err;
-
     const errorMessage = {
-    error: error || 'Not Found',
+    errors: error || 'Not Found',
     status: status || 500,
     message: message,
     timestamp: new Date()
