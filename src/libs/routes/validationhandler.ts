8917f4006b156b8875cd199f1e56df09fa01c692
shivamss454 <shivam.sharma@successive.tech>
2020-02-11 19:31:41 +0530

    fix:remove unwanted commit

--- src/libs/routes/validationhandler.ts
+++ src/libs/routes/validationhandler.ts
@@ -6,25 +6,20 @@ export default function (config) {
         const configkeys = Object.keys(config);
         configkeys.forEach(key => {
             const keydata = config[key];
-            // console.log('*****', keydata);
+            
             const values = keydata.in.map((location) => {
-                // console.log('\n key is',key);
-                // console.log('\n key value is',config[key]);
-                // console.log('\n value is', req[location][key]);
-                // console.log('\n keydata.required', keydata.required);
-
+                
                 if (keydata.required && !req[location][key]) {
                     errArray.push(`${key}  is required`);
                 }
                 else {
                 if (keydata.string && (typeof (req[location][key]) !== 'string')) {
-                    // console.log('------if 111');
+                    
                     errArray.push(`${key}  is should be string`);
                 }
                 if (keydata.number && typeof (req[location][key]) !== 'number') {
                     if (key === 'skip' || key === 'limit') {
-                       // console.log('key---', key, typeof req[location][key],  isNaN(req[location][key]));
-                        // console.log('------parse---', typeof parseInt(req[location][key]))
+                       
                         if (isNaN(req[location][key])) {
                             errArray.push(`${key}  is should be number`);
                         }
@@ -33,9 +28,8 @@ export default function (config) {
                     }
                 }
 
-                // console.log('----regex---', keydata.regex);
+                ;
                 const regex = RegExp(keydata.regex);
-                // console.log('-----regex-----', regex.test((req[location][key])))
                 if (keydata.regex && !regex.test((req[location][key]))) {
                     errArray.push({error: `${key} is not correct`});
                 }
